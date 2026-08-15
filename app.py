c2b286a956b6786e7d58db90fe3e108c1a455c6b
lurno <krrillo1994@gmail.com>
2026-08-15 16:59:59 -0500

    feat: crear aplicación Flask inicial

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__) 
+
+@app.route("/")
+def inicio():
+    return "Bienvenido a Costuras Elegantes"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
